2e0b2bcf86fcc3e35f096de174071aae2a2ad03f
khaueviana <khaueviana@gmail.com>
2018-05-27 12:43:20 -0300

    Add comments

--- iceFactory/index.js
+++ iceFactory/index.js
@@ -34,3 +34,13 @@ cart.addProduct({
 })
 
 console.log(JSON.stringify(cart.getProducts()));
+
+/*We no longer need new. 
+- We just invoke a plain old JavaScript function to create our cart object.
+- We no longer need this. 
+
+- We can access the db object directly from our member functions.
+
+- Our cart object is completely immutable. 
+Object.freeze() freezes the cart object so that new properties can’t be added to it, existing properties can’t be removed or changed, and the prototype can’t be changed either. Just remember that Object.freeze() is shallow, so if the object we return contains an array or another object we must make sure to Object.freeze() them as well. Also, if you’re using a frozen object outside of an ES Module, you need to be in strict mode to make sure that re-assignments cause an error rather than just failing silently.
+*/
